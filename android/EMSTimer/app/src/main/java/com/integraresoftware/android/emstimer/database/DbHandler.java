f2342bcbbf9207b3376e8cd273d439d41b3bab3c
chris <burrusscl@gmail.com>
2014-08-25 10:33:35 -0400

    finished content provider, not tested

--- android/EMSTimer/app/src/main/java/com/integraresoftware/android/emstimer/database/DbHandler.java
+++ android/EMSTimer/app/src/main/java/com/integraresoftware/android/emstimer/database/DbHandler.java
@@ -13,6 +13,18 @@ public class DbHandler extends SQLiteOpenHelper {
 	private static final String TEXT_TYPE = " TEXT, ";
 	private static final String INT_TYPE = " INT, ";
 
+	private static final String SQL_CREATE_TIMES_TABLE =
+			"CREATE TABLE " + TimesContract.TABLE_NAME
+					+ " (" + TimesContract.COLUMN_ID + "INTEGER PRIMARY KEY,"
+					+ TimesContract.COLUMN_DATE + TEXT_TYPE
+					+ TimesContract.COLUMN_ENROUTE + TEXT_TYPE
+					+ TimesContract.COLUMN_ONSCENE + TEXT_TYPE
+					+ TimesContract.COLUMN_CONTACT + TEXT_TYPE
+					+ TimesContract.COLUMN_TRANSPORT + TEXT_TYPE
+					+ TimesContract.COLUMN_HOSPITAL + TEXT_TYPE
+					+ TimesContract.COLUMN_CLEAR + TEXT_TYPE
+					+ TimesContract.COLUMN_VALID + INT_TYPE;
+
 
 	public DbHandler(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -20,24 +32,15 @@ public class DbHandler extends SQLiteOpenHelper {
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		String SQL_CREATE_TIMES_TABLE =
-				"CREATE TABLE " + TimesContract.TABLE_NAME
-						+ " (" + TimesContract.COLUMN_ID + "INTEGER PRIMARY KEY,"
-						+ TimesContract.COLUMN_DATE + TEXT_TYPE
-						+ TimesContract.COLUMN_ENROUTE + TEXT_TYPE
-						+ TimesContract.COLUMN_ONSCENE + TEXT_TYPE
-						+ TimesContract.COLUMN_CONTACT + TEXT_TYPE
-						+ TimesContract.COLUMN_TRANSPORT + TEXT_TYPE
-						+ TimesContract.COLUMN_HOSPITAL + TEXT_TYPE
-						+ TimesContract.COLUMN_CLEAR + TEXT_TYPE
-						+ TimesContract.COLUMN_VALID + INT_TYPE;
-
 		db.execSQL(SQL_CREATE_TIMES_TABLE);
 	}
 
 	@Override
-	public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i2) {
-		//TODO have to work on this one if I want to change it
+	public void onUpgrade(SQLiteDatabase db, int i, int i2) {
+		// Drop the table and delete all data
+		db.execSQL("DROP TABLE IF EXISTS " + TimesContract.TABLE_NAME);
+
+		onCreate(db);
 	}
 
 
--- android/EMSTimer/app/src/main/java/com/integraresoftware/android/emstimer/database/DbProvider.java
+++ android/EMSTimer/app/src/main/java/com/integraresoftware/android/emstimer/database/DbProvider.java
@@ -2,24 +2,89 @@ package com.integraresoftware.android.emstimer.database;
 
 
 import android.content.ContentProvider;
+import android.content.ContentResolver;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
 
 public class DbProvider extends ContentProvider {
-	@Override
-	public boolean onCreate() {
-		return false;
-	}
+	// for debugging
+	public static final String TAG = "DbProvider";
+
+	// content provider and authority
+	public static final String AUTHORITY = "com.integraresoftware.android.emstimer.database.DbProvider";
+	public static final Uri TIMES_CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/times");
+
+	// MIME types for searching
+	public static final String TIMES_MIME_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE
+			+ "/vnd.com.integraresoftware.android.emstimer.database.time";
+	public static final String TIME_MIME_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE
+			+ "/vnd.com.integraresoftware.android.emstimer.database.time";
+
+	// URI matcher
+	private static final int LIST_TIMES = 1;
+	private static final int ITEM_TIME = 2;
+	private static final UriMatcher sUriMatcher = buildMatcher();
+
+	// get database
+	private SQLiteDatabase mDb;
 
 	@Override
-	public Cursor query(Uri uri, String[] strings, String s, String[] strings2, String s2) {
-		return null;
+	public boolean onCreate() {
+		mDb = new DbHandler(getContext()).getReadableDatabase();
+		return true;
+	}
+
+	private static UriMatcher buildMatcher() {
+		UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+		matcher.addURI(AUTHORITY, "time", LIST_TIMES);
+		matcher.addURI(AUTHORITY, "time/#", ITEM_TIME);
+
+		return matcher;
 	}
 
 	@Override
 	public String getType(Uri uri) {
-		return null;
+		switch(sUriMatcher.match(uri)) {
+			case LIST_TIMES:
+				return TIMES_MIME_TYPE;
+			case ITEM_TIME:
+				return TIME_MIME_TYPE;
+			default:
+				throw new IllegalArgumentException("Unknown URI: " + uri);
+		}
+	}
+
+	@Override
+	public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+		// create the query builder
+		SQLiteQueryBuilder queryBuilder = new SQLiteQueryBuilder();
+		// sett the tables the query builder will look into
+		queryBuilder.setTables(TimesContract.TABLE_NAME);
+
+		// determine if it wants a full list or just one
+		int uriType = sUriMatcher.match(uri);
+		switch (uriType) {
+			// wants the full list
+			case LIST_TIMES:
+				break;
+			// wants a specific time
+			case ITEM_TIME:
+				// add the id of the time to get
+				queryBuilder.appendWhere(TimesContract.COLUMN_ID + "=" + uri.getLastPathSegment());
+				break;
+			default:
+				throw new IllegalArgumentException("Unknown Uri: " + uri);
+		}
+
+		Cursor cursor = queryBuilder.query(mDb, projection, selection, selectionArgs, null, null, sortOrder);
+		cursor.setNotificationUri(getContext().getContentResolver(), uri);
+
+		return cursor;
 	}
 
 	@Override
